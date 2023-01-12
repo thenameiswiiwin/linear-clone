5c0a3d6e2b141b02d3e52e4951f6e66d5f71871c
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-01-12 15:53:59 -0600

    style(globals.css, tailwind.config.js): global background colors, text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,14 @@ module.exports = {
     './src/components/**/*.{tsx,jsx,js,ts}',
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: '"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
-      },
-      colors: {
-        background: '#000212',
-      },
+    colors: {
+      black: '#000212',
+      white: '#fff',
     },
+    fontFamily: {
+      sans: '"SF Pro Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif',
+    },
+    extend: {},
   },
   plugins: [],
 };
