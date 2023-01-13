aaed9240a25f16d0a84e2e083b7a8950d83bbe4b
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-01-12 22:14:32 -0600

    feat(Footer): finish repsonsive for Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,9 @@ import Link from 'next/link';
 export const Footer = () => {
   return (
     <footer className="mt-12 border-t border-white-200 py-[5.6rem] text-sm">
-      <Container className="flex justify-between">
+      <Container className="flex flex-col justify-between md:flex-row">
         <div>
-          <div className="flex h-full flex-col">
+          <div className="flex h-full flex-row justify-between lg:flex-col">
             <div className="flex items-center text-gray-100">
               <Logo className="mr-4 h-4 w-4" /> Linear - Designed Worldwide
             </div>
@@ -22,9 +22,12 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="flex">
+        <div className="flex flex-wrap">
           {FooterLinks.map((column) => (
-            <div key={column.title} className="min-w-[18rem]">
+            <div
+              key={column.title}
+              className="mt-10 min-w-[50%] lg:mt-0 lg:min-w-[18rem]"
+            >
               <h3 className="mb-3 font-medium">{column.title}</h3>
               <ul>
                 {column.links.map((link) => (
