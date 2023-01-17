236eca2d4d990552ea9ac21ea074530d0c7f045b
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-01-16 22:59:05 -0600

    feat(HeroImage): tilt animation when image get in view

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import HeroImg from '@assets/images/hero.webp';
+import clsx from 'clsx';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
 
 export const HeroImage = () => {
+  const { ref, inView } = useInView({ threshold: 0.4, triggerOnce: true });
+
+  console.log(inView);
+
   return (
-    <div className="mt-[12.8rem] overflow-hidden [perspective:2000px]">
-      <div className="rounded-lg border border-gray-100 bg-white bg-opacity-[0.01] bg-hero-gradient [transform:rotateX(25deg)]">
+    <div
+      ref={ref}
+      className="mt-[12.8rem] overflow-hidden [perspective:2000px]"
+    >
+      <div
+        className={clsx(
+          'rounded-lg border border-gray-100 bg-white bg-opacity-[0.01] bg-hero-gradient',
+          inView ? 'animate-image-rotate' : '[transform:rotateX(25deg)]'
+        )}
+      >
         <Image src={HeroImg} alt="Hero image" />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
   theme: {
     animation: {
       'fade-in': 'fade-in 1000ms var(--animation-delay, 0ms) ease forwards',
+      'image-rotate': 'image-rotate 1400ms ease forwards',
     },
     backgroundImage: {
       'hero-gradient':
@@ -53,6 +54,12 @@ module.exports = {
       'fade-in': {
         from: { opacity: 0, transform: 'translateY(-10px)' },
         to: { opacity: 1, transform: 'none' },
+      },
+      'image-rotate': {
+        '0%': { transform: 'rotateX(25deg)' },
+        '25%': { transform: 'rotateX(25deg) scale(0.9)' },
+        '60%': { transform: 'none' },
+        '100%': { transform: 'none' },
       },
     },
     spacing: {
