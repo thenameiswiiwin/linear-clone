df69d8e60cb950c48bbf3b384ee6e812322373d9
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-22 00:06:10 -0600

    feat(Features): added gradient divider line

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -36,9 +36,10 @@ const FeatureMain = ({ image, text }: FeatureMainProps) => {
       <div className="relative rounded-[14px] before:absolute before:inset-0 before:rounded-[inherit] before:bg-[linear-gradient(rgba(255,255,255,0.3),rgba(255,255,255,0)120%)] before:p-[1px] before:[mask:linear-gradient(black,black)content-boxcontent-box,linear-gradient(black,black)] before:[mask-composite:xor] after:absolute after:inset-0 after:rounded-[inherit] after:bg-[rgba(255,255,255,0.15)] after:[mask:linear-gradient(black,transparent)]">
         <Image src={image} alt="issues" className="h-auto w-full" />
       </div>
-      <p className="mx-auto mt-16 w-[80%] text-4xl leading-tight text-white">
+      <p className="mx-auto my-16 w-[80%] text-4xl leading-tight text-white">
         {text}
       </p>
+      <hr className="mb-[7.2rem] h-[1px] border-none bg-[linear-gradient(to_right,transparent,rgba(255,255,255,0.1)50%,transparent)]" />
     </div>
   );
 };
