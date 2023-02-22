ae158342b3c3ed7cd483e6a0cfdfbdcd4826aeea
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-21 18:34:19 -0600

    feat(CommandMenu): contents and some styling

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -1,7 +1,104 @@
+const CommandOptions = [
+  {
+    label: 'Assign to...',
+    icon: () => {},
+    subOptions: [
+      {
+        label: 'Jori',
+        icon: () => {},
+      },
+      {
+        label: 'Karri',
+        icon: () => {},
+      },
+      {
+        label: 'Tuomas',
+        icon: () => {},
+      },
+    ],
+  },
+  {
+    label: 'Change status...',
+    icon: () => {},
+    subOptions: [
+      {
+        label: 'Backlog',
+        icon: () => {},
+      },
+      {
+        label: 'Todo',
+        icon: () => {},
+      },
+      {
+        label: 'In Progress',
+        icon: () => {},
+      },
+      {
+        label: 'Done',
+        icon: () => {},
+      },
+    ],
+  },
+  {
+    label: 'Change priority...',
+    icon: () => {},
+    subOptions: [
+      {
+        label: 'No priority',
+        icon: () => {},
+      },
+      {
+        label: 'Urgent',
+        icon: () => {},
+      },
+      {
+        label: 'High',
+        icon: () => {},
+      },
+      {
+        label: 'Medium',
+        icon: () => {},
+      },
+    ],
+  },
+  {
+    label: 'Add label...',
+    icon: () => {},
+    subOptions: [
+      {
+        label: 'Bug',
+        icon: () => {},
+      },
+      {
+        label: 'Feature',
+        icon: () => {},
+      },
+      {
+        label: 'Improvement',
+        icon: () => {},
+      },
+    ],
+  },
+];
+
 export const CommandMenu = () => {
   return (
-    <div className="absolute top-1/2 left-1/2 w-[90vw] max-w-[64rem] -translate-x-1/2 -translate-y-1/2 rounded-lg border border-gray-100 bg-gray-100 p-8">
-      Command Menu
+    <div className="absolute top-1/2 left-1/2 flex w-[90vw] max-w-[64rem] -translate-x-1/2 -translate-y-1/2 flex-col items-start rounded-lg border border-gray-100 bg-gray-100">
+      <span className="ml-4 mt-2 bg-white/[0.05] px-2 text-xs leading-10 text-white/50">
+        LIN-111 Walkway lighting
+      </span>
+      <input
+        type="text"
+        placeholder="Type a command or search..."
+        className="w-full bg-transparent p-5 text-lg outline-none"
+      />
+      <div className="flex flex-col text-sm text-gray-200">
+        {CommandOptions.map((option) => (
+          <button key={option.label} type="button">
+            {option.label}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/sections/UnlikeAnyTools.tsx
+++ src/components/sections/UnlikeAnyTools.tsx
@@ -62,14 +62,12 @@ export const UnlikeAnyTools = () => (
             <ChevronIcon fill="#8A8F98" className="ml-1" />
           </Button>
         </div>
-        <div className="relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-end rounded-[4.8rem] border border-gray-100 bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
+        <div className="relative flex min-h-[48rem] w-full shrink-0 snap-center flex-col items-center justify-start rounded-[4.8rem] border border-gray-100 bg-glass-gradient p-8 text-center md:max-w-[calc(66.66%-12px)] md:basis-[calc(66.66%-12px)] md:p-14">
+          <p className="mb-4 text-3xl">Meet your command line</p>
+          <p className="text-md text-gray-300">
+            Complete any action in seconds with the global command menu.
+          </p>
           <CommandMenu />
-          <div>
-            <p className="mb-4 text-3xl">Meet your command line</p>
-            <p className="text-md text-gray-300">
-              Complete any action in seconds with the global command menu.
-            </p>
-          </div>
         </div>
       </div>
     </div>
