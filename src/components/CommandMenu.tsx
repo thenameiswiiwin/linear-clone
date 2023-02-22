2706b6b398387b39dd25f77aa90e7519f6fffbbe
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-21 19:40:43 -0600

    feat(CommandMenu): first button is always highlighted

--- src/components/CommandMenu.tsx
+++ src/components/CommandMenu.tsx
@@ -172,7 +172,7 @@ export const CommandMenu = () => {
               onClick={() => {
                 setSelectedOption('subOptions' in menuItem ? index : null);
               }}
-              className="commandMenuBtn flex h-[4.6rem] w-full items-center gap-3 px-5 hover:bg-white/[0.05]"
+              className="commandMenuBtn flex h-[4.6rem] w-full items-center gap-3 px-5 first:bg-white/[0.15] hover:bg-white/[0.05]"
             >
               <Icon />
               {label}
