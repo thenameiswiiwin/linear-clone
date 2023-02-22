b508143f9f9834a88bc5536a1ba16f96a133ffa6
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-22 11:26:15 -0600

    fix: fixed unsynchronized lock files and svg invalid Dom property

---
+++ src/components/icons/features/linearWorkflows/index.tsx
@@ -0,0 +1,7 @@
+export * from './airbyte';
+export * from './figma';
+export * from './frontInterZen';
+export * from './git';
+export * from './plus';
+export * from './sentry';
+export * from './slackDisc';
---
+++ src/components/icons/features/linearWorkflows/plus.tsx
@@ -0,0 +1,73 @@
+export const PlusIcon = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="88" height="88" fill="none">
+    <g filter="url(#a)">
+      <path
+        fill="url(#b)"
+        fill-opacity="0.8"
+        fillRule="evenodd"
+        d="M43.809 81.206c20.656 0 37.402-16.746 37.402-37.403 0-20.656-16.746-37.402-37.402-37.402-20.657 0-37.403 16.745-37.403 37.402S23.152 81.206 43.81 81.206zm-.78-57.662a3.896 3.896 0 00-3.896 3.896v13.246H27.445a3.896 3.896 0 000 7.792h11.688v11.689a3.896 3.896 0 107.792 0V48.478h13.247a3.896 3.896 0 000-7.792H46.925V27.44a3.896 3.896 0 00-3.896-3.896z"
+        clipRule="evenodd"
+      ></path>
+    </g>
+    <path
+      stroke="url(#c)"
+      stroke-opacity="0.2"
+      stroke-width="1.558"
+      d="M80.432 43.803c0 20.227-16.397 36.623-36.623 36.623-20.227 0-36.624-16.396-36.624-36.623C7.185 23.577 23.582 7.18 43.81 7.18c20.226 0 36.623 16.397 36.623 36.623zM43.029 22.764a4.675 4.675 0 00-4.675 4.676v12.467H27.445a4.675 4.675 0 100 9.35h10.91v10.91a4.675 4.675 0 109.35 0v-10.91h12.467a4.675 4.675 0 100-9.35H47.705V27.44a4.675 4.675 0 00-4.676-4.676z"
+    ></path>
+    <defs>
+      <linearGradient
+        id="b"
+        x1="43.809"
+        x2="43.809"
+        y1="6.401"
+        y2="81.206"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#6C38FF"></stop>
+        <stop offset="1" stop-color="#B59AFF"></stop>
+      </linearGradient>
+      <linearGradient
+        id="c"
+        x1="43.809"
+        x2="43.809"
+        y1="6.401"
+        y2="81.206"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#fff"></stop>
+        <stop offset="1" stop-color="#fff" stop-opacity="0"></stop>
+      </linearGradient>
+      <filter
+        id="a"
+        width="74.805"
+        height="74.805"
+        x="6.406"
+        y="6.401"
+        color-interpolation-filters="sRGB"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood flood-opacity="0" result="BackgroundImageFix"></feFlood>
+        <feBlend
+          in="SourceGraphic"
+          in2="BackgroundImageFix"
+          result="shape"
+        ></feBlend>
+        <feColorMatrix
+          in="SourceAlpha"
+          result="hardAlpha"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+        ></feColorMatrix>
+        <feOffset dy="3.117"></feOffset>
+        <feComposite
+          in2="hardAlpha"
+          k2="-1"
+          k3="1"
+          operator="arithmetic"
+        ></feComposite>
+        <feColorMatrix values="0 0 0 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.1 0"></feColorMatrix>
+        <feBlend in2="shape" result="effect1_innerShadow_1678_93891"></feBlend>
+      </filter>
+    </defs>
+  </svg>
+);
