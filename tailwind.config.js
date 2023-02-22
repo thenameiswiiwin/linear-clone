cc3a7b26ad4e68efe6a367c8e096d67f461539a5
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-22 03:35:39 -0600

    fix: fixed cut off title in featureMain

--- tailwind.config.js
+++ tailwind.config.js
@@ -62,9 +62,9 @@ module.exports = {
       '3xl': '2.6rem',
       '4xl': '3.2rem',
       '5xl': '4rem',
-      '6xl': ['4.4rem', '1'],
-      '7xl': ['4.8rem', '1'],
-      '8xl': ['8rem', '1'],
+      '6xl': ['4.4rem', '1.1'],
+      '7xl': ['4.8rem', '1.1'],
+      '8xl': ['8rem', '1.1'],
     },
     keyframes: {
       bounce: {
